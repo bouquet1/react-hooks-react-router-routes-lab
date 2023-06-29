a4311b006efee6345ffcc1b563c4b553f8fc5e2f
bouquet1 <bukuleta@gmail.com>
2023-06-28 19:31:01 -0700

    Movies gives an error

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,8 +1,28 @@
 import React from "react";
 import { movies } from "../data";
+import { v4 as uuidv4} from "uuid";
 
-function Movies() {
-  return <div>{/*{code here}*/}</div>;
+function Movies({movies}) {
+
+  const movieList = movies.map((movie) => (
+    <div key={uuidv4}>
+      <h2>{movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <ul>
+        {movie.genres.map((genre, index) => (
+          <li key={uuidv4}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movieList}
+  </div>
+    )
 }
 
 export default Movies;
+
