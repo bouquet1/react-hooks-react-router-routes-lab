cc3a5942cc651397b24b52106a332e4ca939ba2e
bouquet1 <bukuleta@gmail.com>
2023-06-29 10:37:48 -0700

    Directors/ Actors done test passed

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -3,18 +3,24 @@ import { movies } from "../data";
 
 function Movies() {
 
-  const movieList = movies.map((movie) => {
-    return <div key={movie.title}>{movie.title}{movie.time}</div>})
-  
-  const genreList = movies.map(movie => (<li key={movie.title}>{movie.genres}</li>))
+      
+  const genreList = movies.map(movie => {
+    return (<li key={movie.title}>{movie.genres}</li>
+  )})
+
+  const movieList = movies.map((movie, index) => {
+    return <div key={index}>
+      <h2>Name: {movie.title}</h2>
+      <p>Time: {movie.time}</p>
+      <h4>Genres:</h4>
+      <ul>{genreList}</ul>
+    </div>})
+
 
   return (
   <div>
     <h1>Movies Page</h1>
       {movieList}
-    <ul>
-      {genreList}
-    </ul>
   </div>
     )
 }
